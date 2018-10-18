f960f2f1451c60a804e3d3d2881c28f6c64fffa0
Akanksha <akankshamittal0110@gmail.com>
2018-10-18 10:34:41 +0530

    Changed dashboard css

--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -5,32 +5,34 @@
   <title>BitStream</title>
   
 
-   <link rel='stylesheet prefetch' href='https://fonts.googleapis.com/css?family=Open+Sans:600'>
-
-
+<link rel='stylesheet prefetch' href='https://fonts.googleapis.com/css?family=Open+Sans:600'>
 <link href="{{asset('css/bootstrap.min.css')}}" rel="stylesheet"/>
 <link href="{{asset('css/features_boxed.css')}}" rel="stylesheet"/>
 <link href="{{asset('css/navigation_button.css')}}" rel="stylesheet"/>
 <link href="{{asset('css/styles.css')}}" rel="stylesheet"/>
-
 <script src="{{asset('js/jquery.min.js')}}" async defer></script>
-
+<meta name="viewport" content="width=device-width, initial-scale=1.0">  
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=ABeeZee">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Alfa+Slab+One">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Aubrey">
+  
 </head>
+
 <body>
     <div>
-        <nav class="navbar navbar-light navbar-expand-md navigation-clean-button">
-            <div class="container"><a class="navbar-brand" href="#">BitStream</a><button class="navbar-toggler" data-toggle="collapse" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
-                <div class="collapse navbar-collapse"
-                    id="navcol-1">
+        <nav class="navbar navbar-light navbar-expand-md navigation-clean-button" style="background-color:rgb(17,1,1);">
+            <div class="container"><a class="navbar-brand" href="#" style="color:#108c76;font-family:'Alfa Slab One', cursive;">BitStream</a><button class="navbar-toggler" data-toggle="collapse" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
+                <div
+                    class="collapse navbar-collapse" id="navcol-1">
                     <ul class="nav navbar-nav mr-auto">
-                        <li class="nav-item" role="presentation"><a class="nav-link active" href="#">Practice</a></li>
-                        <li class="nav-item" role="presentation"><a class="nav-link" href="#">Compete</a></li>
+                        <li class="nav-item" role="presentation"><a class="nav-link active" href="#" style="font-family:ABeeZee, sans-serif;color:#f9efc1;">Practice</a></li>
+                        <li class="nav-item" role="presentation"><a class="nav-link" href="#" style="color:#f9efc1;font-family:ABeeZee, sans-serif;">Compete</a></li>
                         <li class="dropdown"><a class="dropdown-toggle nav-link dropdown-toggle" data-toggle="dropdown" aria-expanded="false" href="#"></a>
                             <div class="dropdown-menu" role="menu"><a class="dropdown-item" role="presentation" href="#">First Item</a><a class="dropdown-item" role="presentation" href="#">Second Item</a><a class="dropdown-item" role="presentation" href="#">Third Item</a></div>
                         </li>
                     </ul><span class="navbar-text actions"> </span></div>
-            </div>
-        </nav>
+    </div>
+    </nav>
     </div>
     <div class="features-boxed"><div class="container">
     <div class="intro">
@@ -50,8 +52,8 @@
         </div>
         <div class="col-sm-6 col-md-5 col-lg-4 item">
             <div class="box"><img />
-                <h3 class="name">Algorithms </h3>
-                <p class="description"><br /><strong>Algorithms </strong>can perform calculation, data processing and automated reasoning tasks and builds analytical abilities.<strong>Algorithm </strong>can be expressed within a finite amount of space and time .<br /><br /></p><button class="btn btn-primary"
+                <h3 class="name">Algorithms</h3>
+                <p class="description"><br /><strong>Algorithms </strong>can perform calculation, data processing and automated reasoning tasks.<strong>Algorithm </strong>can be expressed within a finite amount of space and time .<br /><br /><br/></p><button class="btn btn-primary"
                     type="button">Practice</button></div>
         </div>
     </div>
@@ -59,3 +61,5 @@
     <script src="assets/js/jquery.min.js"></script>
     <script src="assets/bootstrap/js/bootstrap.min.js"></script>
 </body>
+
+</html>
